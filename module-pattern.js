1c1304b2b5b8b4b9b86cb3587ffb7f289b232634
Jamie Stewart <jamstewart@gmail.com>
2011-07-21 19:26:12 +0100

    Updated to show overriding of a module

--- module-pattern.js
+++ module-pattern.js
@@ -1,33 +1,38 @@
-/* Normal Module */
+/* Should be in a more global part of the framework, here for ease atm */
+Object.prototype.clone = function (){
+	function Clone() {}
+    	Clone.prototype = this;
+    	return new Clone();
+}
 
+/* Normal Module */
 var module = (function(){
-	var module={},privateVar = 'private';
+	var module={}, // Return module for public object
+	privateVar = 'private'; // Private variable
 	
 
-	module.publicVar = 'public';
-	module.publicFunc = function(){console.log('public')};
+	module.publicVar = 'public'; // Public variable
+	module.publicFunc = function(){console.log('public')}; // Public function
 
-	return module;
+	return module; // Return public object
 })();
 
 console.log(module);
 
 /* Module Pattern Inheritance */
 var inhmodule = (function(base){
-	var inhmodule = {},key;
-	
-	inhmodule = clone(base);
+	var inhmodule = {}, // Return module for public object
 
-	inhmodule.newPublicFunc = function(){ console.log('newPublicfunc'); };
-	return inhmodule;
+	inhmodule = base.clone(); // Clone object to allow for inheritance
+
+	inhmodule.newPublicFunc = function(){ console.log('newPublicfunc'); }; // Public function to demo extension
+	inhmodule.publicVar = "New public"; // Demo to show overriding of a variable
+	inhmodule.publicFunc = function (){ console.log("New public func") } // Demo to show overriding of a function
+	
+	return inhmodule; // Return public object
 	
 })(module);
 
+
 console.log(inhmodule);
 
-/* Should be in a more global part of the framework, here for ease atm */
-function clone(object){
-	function Clone() {}
-    	Clone.prototype = object;
-    	return new Clone();
-}
